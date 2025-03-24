3d3babf7f6ce4cc3fb276ee6d92157163e186b81
Thomas Bastos <thomas.felip16@gmail.com>
2025-03-24 13:32:16 -0300

    feat: register with validate

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,28 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
-   
+    // Registrar usuário
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'uuid' => \Illuminate\Support\Str::uuid(),
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'Usuário registrado com sucesso!',
+            'uuid' => $user->uuid
+        ], 201);
+    }
+
     
 
    
