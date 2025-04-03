75a1e22c03450f64aef2330ebc3d4834c04bd8d4
Thomas Bastos <thomas.felip16@gmail.com>
2025-04-03 09:34:15 -0300

    feat: create routes index data for trainings

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
     // Outras rotas autenticadas
+    Route::get('/trainings', [TrainingController::class, 'index']);
 
 
 });
