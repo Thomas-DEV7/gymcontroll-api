4ee27b51e0b9619babe275879491be5cf8e8a901
Thomas Bastos <thomas.felip16@gmail.com>
2025-04-03 09:32:37 -0300

    feat: create route me, live getter my data (user logged)

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
     // Outras rotas autenticadas
 
 
