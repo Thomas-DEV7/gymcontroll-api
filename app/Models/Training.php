b005a9dac6bd3195f32e90baa17742f02b65ff14
Thomas Bastos <thomas.felip16@gmail.com>
2025-04-03 09:39:25 -0300

    feat: create model Trainings

--- app/Models/Training.php
+++ app/Models/Training.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Training extends Model
 {
-    use HasFactory;
+    protected $fillable = ['uuid', 'name', 'user_id'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->uuid = (string) Str::uuid();
+        });
+    }
+
+    // Relacionamentos
+    public function exercises()
+    {
+        return $this->hasMany(Exercise::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
